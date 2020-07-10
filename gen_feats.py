d9655fd4aa70d6b106116c76c5044cda2a9bd3b7
zhenbing <zjh@cnic.cn>
2020-07-10 09:59:28 +0800

    create training dataset

--- gen_feats.py
+++ gen_feats.py
@@ -18,24 +18,27 @@ def read_img_data_dict(imgs_dir):
         img_path = os.path.join(imgs_dir, img_name)
         ds = rasterio.open(img_path)
         img = ds.read()
-        imgs_data.append(img)
+        imgs_data.append(img[None, ...])
     imgs_data = np.concatenate(imgs_data, axis=0)
     
     return imgs_data
 
 if __name__ == '__main__':    
-    BASE_DIR = '/mnt/rsimages/lulc/AISample'
+    BASE_DIR = '/mnt/rsimages/lulc/AISample'    
+    
+    X_train = read_img_data_dict(os.path.join(BASE_DIR,"train_set/imgs"))
+    print(X_train.shape)
     X_train_file = os.path.join(BASE_DIR,'cache/train-set/X_train.p')
     print(X_train_file)
-    X_train = read_img_data_dict(os.path.join(BASE_DIR,"train_set/imgs"))
     pickle.dump(X_train, open(X_train_file,'wb'))
-    
+      
     Y_train = read_img_data_dict(os.path.join(BASE_DIR,'train_set/gt'))
     Y_train_file = os.path.join(BASE_DIR,'cache/train-set/Y_train.p')
     pickle.dump(Y_train, open(Y_train_file,'wb'))
     
     X_test = read_img_data_dict(os.path.join(BASE_DIR,"test_set/imgs"))
     X_test_file = os.path.join(BASE_DIR,'cache/test-set/X_test.p')
+    print(X_test.shape)
     pickle.dump(X_test, open(X_test_file,'wb'))
     
     Y_test = read_img_data_dict(os.path.join(BASE_DIR,'test_set/gt'))
